c5c1f286a89c0a913870d6fad0db4fd99724bf78
Victor R <39545521+viicslen@users.noreply.github.com>
2026-02-10 17:43:26 -0500

    fix: reorder inputs and modules for better structure in home.nix

--- hosts/dostov-dev/home.nix
+++ hosts/dostov-dev/home.nix
@@ -1,26 +1,13 @@
 {
-  inputs,
-  pkgs,
   lib,
+  pkgs,
+  inputs,
+  osConfig,
   ...
 }: {
   home.autostart = [
     pkgs.jetbrains-toolbox
   ];
-
-  home.file.".config/hypr/pyprland.toml".text = lib.mkAfter ''
-    [monitors.placement."LW9AA0048525"]
-    rightOf = "DP-1"
-    transform = 3
-  '';
-
-  wayland.windowManager.hyprland.settings = {
-    monitor = [
-      "DP-1, 1920x1080@59.99, 0x0, 1, vrr, 0"
-      "DP-2, 1920x1080@59.99, 1920x0, 1, transform, 3, vrr, 0"
-      ", preferred, auto, 1"
-    ];
-  };
 
   modules.programs = {
     ray.enable = true;
@@ -30,7 +17,22 @@
     vivaldi.enable = true;
   };
 
-  programs.niri.settings = {
+
+  home.file.".config/hypr/pyprland.toml".text = lib.mkAfter ''
+    [monitors.placement."LW9AA0048525"]
+    rightOf = "DP-1"
+    transform = 3
+  '';
+
+  wayland.windowManager.hyprland.settings = lib.mkIf osConfig.programs.hyprland.enable {
+    monitor = [
+      "DP-1, 1920x1080@59.99, 0x0, 1, vrr, 0"
+      "DP-2, 1920x1080@59.99, 1920x0, 1, transform, 3, vrr, 0"
+      ", preferred, auto, 1"
+    ];
+  };
+
+  programs.niri.settings = lib.mkIf osConfig.programs.niri.enable {
     outputs = {
       "DP-1" = {
         scale = 1.0;
