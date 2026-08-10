8dbf775d85d581c5204b2e5fd8094a69a7b2756b
Victor R <39545521+viicslen@users.noreply.github.com>
2026-08-10 08:40:29 -0400

    Add default keybinds for herdr

--- modules/home-manager/programs/herdr/default.nix
+++ modules/home-manager/programs/herdr/default.nix
@@ -14,7 +14,28 @@
         onboarding = false;
         keys = {
           prefix = "ctrl+space";
+          workspace_picker = "prefix+w";
+          detach = "prefix+d";
+          reload_config = "prefix+r";
+          open_notification_target = "prefix+shift+o";
+          previous_workspace = "prefix+(";
+          next_workspace = "prefix+)";
+          rename_tab = "prefix+comma";
+          previous_tab = [
+            "prefix+p"
+            "prefix+ctrl+p"
+            "prefix+ctrl+h"
+          ];
+          next_tab = [
+            "prefix+n"
+            "prefix+ctrl+n"
+            "prefix+ctrl+l"
+          ];
+          close_tab = "prefix+&";
+          last_pane = "prefix+semicolon";
+          cycle_pane_next = "prefix+o";
           split_vertical = "prefix+|";
+          resize_mode = "prefix+shift+r";
         };
       };
     };
